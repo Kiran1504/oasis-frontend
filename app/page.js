ff1708a6739311a7970d380e131901e751281a3c
pma2012 <anapatprabhu@gmail.com>
2024-04-20 20:56:36 +0530

    changes made

--- app/page.js
+++ app/page.js
@@ -2,38 +2,7 @@ import { IoMdPeople } from "react-icons/io";
 
 export default function Home() {
   return (
-    <>
-      <div className="container bg--600 h-screen w-full  bg-black ">
-        <div className="header bg-black h-[20%] w-full text-white flex justify-between items-center px-[3vw] z-[9999]">
-          <div className="head-left">
-            <h1 className="text-[25px] font-bold">Home</h1>
-          </div>
-          <div className="search h-[100%] w-[20vw] bg--500 flex justify-center items-center ">
-                <input type="text" placeholder="Search" className="h-[40px] w-[100%] rounded-[50px] px-[20px] bg-transparent border-[1.5px] border-solid border-[#848484] text-white"/>
-              </div>
-          <div className="head-right">
-              <h2>User_101</h2>
-          </div>
-        </div>
-        <div className="main-content h-[80%] w-[100%] bg--900 px-[] ">
-          <div className="left-post relative h-[100%] w-[70%] bg--600 px-[]">
-
-              
-
-              <div className="post-container h-[100%] w-full bg--500 grid-flow-col space-y-[6vw]  p-[2vw] overflow-y-scroll  scroll-smooth z-[2]">
-                <div className="post h-[100%] w-[100%] border-blue-500 border-[2px] rounded-[30px] bg-[#0c0c0c] ">
-                </div>
-                <div className="post h-[100%] w-[100%] border-blue-500 border-[2px] rounded-[30px] ">
-                </div>
-                <div className="post h-[100%] w-[100%] border-blue-500 border-[2px] rounded-[30px] ">
-                </div>
-              </div>
-          </div>
-          <div className="right-filters">
-
-          </div>
-        </div>
-      </div>
-    </>
+   <>
+   </>
   );
 }
--- components/ComunityCard.jsx
+++ components/ComunityCard.jsx
@@ -4,8 +4,10 @@ import { HiUsers } from "react-icons/hi";
 export default function ComunityCard() {
   return (
     <>
-      <div
-        className=" m-6 md:m-0 bg-[#1E1F24] p-5   sm:grid grid-cols-12 gap-2 sm:gap-8 my-7 
+    <div className=" my-7">
+           
+    <div
+        className=" m-6  md:m-0 bg-[#1E1F24] p-5   sm:grid grid-cols-12 gap-2 sm:gap-8
          border-[1.5px] rounded-3xl  border-[#767676]  md:mr-16 "
       >
         <div className="hidden sm:block col-span-4 my-auto ">
@@ -13,42 +15,33 @@ export default function ComunityCard() {
         </div>
         <div className="  col-span-8 my-4">
           <div className=" mb-3">
-            
             <div className=" flex justify-between   ">
               <div className="sm:hidden w-1/3  ">
                 <img src="community_profile.png" alt="" />
               </div>
 
-
-              
               <div className=" ml-6 sm:ml-0 flex w-full justify-between ">
-              <p className=" text-[#00B2FF] font-bold text-3xl sm:text-4xl md:text-5xl my-auto">
-                IEEE
-              </p>
-              <div className="hidden  sm:my-auto sm:flex">
-                <div className=" my-auto">
-                  <HiUsers color="white " fill="white" fontSize="20px" />
+                <p className=" text-[#00B2FF] font-bold text-3xl sm:text-4xl md:text-5xl my-auto">
+                  IEEE
+                </p>
+                <div className="hidden  sm:my-auto sm:flex">
+                  <div className=" my-auto">
+                    <HiUsers color="white " fill="white" fontSize="20px" />
+                  </div>
+
+                  <p className=" text-white  text-lg font-medium ml-2">12k</p>
                 </div>
 
-                <p className=" text-white  text-lg font-medium ml-2">12k</p>
-              </div>
+                <p className=" text-white my-auto border-[#767676]  border-[1.5px] rounded-3xl px-2 sm:px-3 text-sm sm:text-md ">
+                  follow +
+                </p>
 
-              <p className=" text-white my-auto border-[#767676]  border-[1.5px] rounded-3xl px-2 sm:px-3 text-sm sm:text-md ">
-                follow +
-              </p>
-
-              <div className=" my-auto">
-                <HiDotsVertical color="white" fontSize="25px" />
+                <div className=" my-auto">
+                  <HiDotsVertical color="white" fontSize="25px" />
+                </div>
               </div>
-              
-              </div>
-              
-             
             </div>
-            
-            
-            
-            
+
             <div className=" flex mt-3">
               <p className=" text-[#828282] text-sm sm:text-md md:text-lg">
                 Public
@@ -73,6 +66,8 @@ export default function ComunityCard() {
           </p>
         </div>
       </div>
+    </div>
+      
     </>
   );
 }
